5c95a4c4ee6c48eab9c5662602ef1f7c7ce45de0
Katie Moosman <moosman.katie@gmail.com>
2019-03-18 22:25:40 -0700

    created migrations and models

---
+++ app/models/User.rb
@@ -0,0 +1,5 @@
+class User < ActiveRecord::Base
+    has_many :destinations
+    has_secure_password
+  end
+  
---
+++ db/migrate/20190319050943_create_destinations.rb
@@ -0,0 +1,12 @@
+class CreateDestinations < ActiveRecord::Migration
+  def change
+    create_table :destinations do |t|
+      t.string :name
+      t.string :location
+      t.string :dates
+      t.string :image_path
+      t.string :price 
+      t.string :user_id
+    end
+  end
+end
